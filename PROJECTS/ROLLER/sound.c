caee37a1b47dc6acd0093f10a9d82b280f6ac25e
Zizin13 <thespengo@gmail.com>
2026-05-05 22:07:17 -0700

    undoing commit e986f1d

--- PROJECTS/ROLLER/sound.c
+++ PROJECTS/ROLLER/sound.c
@@ -1596,10 +1596,6 @@ void readsoundconfig(void)
   if (!fp) 
   {
     autoselectsoundlanguage(); // Add by ROLLER to auto-select languagename
-    //added by ROLLER to ensure sound is always available
-    if (!SoundCard)
-      SoundCard = -1;
-    return;
   }
 
   // Determine file size
